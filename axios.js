195b6d15620f50f267b6acadf2a15520e508a05c
Siddesh <siddesh@Siddeshs-MacBook-Pro.local>
2020-11-05 14:15:29 -0500

    added comments

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 //library for making http requests. certain advantages over fetch including it parses data so do not have to use JSON function to parse 
 //include axios cdn in html file before app.js script to be able to use
 axios
-	.get('https://swapi.dev/api/planets/')
+	.get('https://swapi.dev/api/planets/') //gets a promise which will have 2 callbacks associated with it .then and .catch
 	.then((res)=>{
 		console.log(res.data);
 	})
